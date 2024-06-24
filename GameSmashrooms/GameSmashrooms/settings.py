9cd4f0c0886e2a495ae1ac54f7273f0ee58ab6d1
Benka <bene.munoz@duocuc.cl>
2024-06-24 01:51:30 -0400

    1.2

--- GameSmashrooms/GameSmashrooms/settings.py
+++ GameSmashrooms/GameSmashrooms/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-o462$wx9gi^_r)mw^&-67rie3n#mynrtw+!+aiw8a+!p%g&502
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 LOGIN_REDIRECT_URL = '/'
